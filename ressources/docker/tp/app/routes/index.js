6c13f14fd22859d89fa0b56e91b1ef3eada30c88
paulhiv <paul.hivert@ynov.com>
2020-08-23 03:01:49 +0200

    added bonus

--- ressources/docker/tp/app/routes/index.js
+++ ressources/docker/tp/app/routes/index.js
@@ -2,26 +2,39 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 let success
+let fail
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-/* check mysql connection */
+/* mysql connection */
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'mysql-container',
   user     : 'user',
   password : 'password',
   database : 'mysqldb'
 });
 
 /* GET bonus page .*/
-router.get('/', function(req, res, next) {
-  res.render('bonus', success);
+connection.connect(function(err) {
+  if (err) {
+    fail = err;
+    success = err;
+  }else {
+  success = connection.threadId;
+  }
 });
 
 
+router.get('/bonus', function(req, res, next) {
+  if (fail == success) {
+    res.render('fail', {title: fail})
+  }else {
+  res.render('bonus', {title: success});
+  }
+});
 
 module.exports = router;
